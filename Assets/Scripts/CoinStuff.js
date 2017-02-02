0a0424d57c5ee887ba63f38aa392ca646b16d2a9
Pranjal <ug201314013@iitj.ac.in>
2017-02-03 01:33:22 +0530

    Assets

---
+++ Assets/Scripts/CoinStuff.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+public var points : int = 5;
+
+function OnTriggerEnter2D(other : Collider2D)
+{
+	Debug.Log("Triggered worth : " + points);
+
+	Destroy(gameObject);
+	// gameObject refers to the object to which the script is attached to 
+}
